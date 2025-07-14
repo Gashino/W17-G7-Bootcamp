bde04c05a325a8a043a4172a96207cfc530d2379
agaggino_meli <axel.gaggino@mercadolibre.com>
2025-07-14 12:19:19 -0300

    feat: -Remove unused method. -Add update method sql to products

--- internal/repository/product_map.go
+++ internal/repository/product_map.go
@@ -60,6 +60,18 @@ const (
 		height,
 		length
 	) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+	Update = `UPDATE products SET
+		product_code = COALESCE(?, product_code),
+		description = COALESCE(?, description),
+		net_weight = COALESCE(?, net_weight),
+		expiration_rate = COALESCE(?, expiration_rate),
+		recommended_freezing_temperature = COALESCE(?, recommended_freezing_temperature),
+		freezing_rate = COALESCE(?, freezing_rate),
+		product_type_id = COALESCE(?, product_type_id),
+		seller_id = COALESCE(?, seller_id),
+		width = COALESCE(?, width),
+		height = COALESCE(?, height),
+		length = COALESCE(?, length) WHERE id = ?`
 )
 
 func (p ProductSql) GetAll() map[int]models.Product {
@@ -158,24 +170,7 @@ func (p ProductSql) Create(product models.Product) (*models.Product, error) {
 		product.Length,
 	)
 	if err != nil {
-		var errorResponse pkg.ServiceError
-		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
-			switch mysqlErr.Number {
-			case 1062:
-				errorResponse = pkg.ServiceErrors[pkg.ErrConflict]
-				errorResponse.Message = "product_code already exists"
-				return nil, errorResponse
-			case 1452:
-				errorResponse = pkg.ServiceErrors[pkg.ErrNotFound]
-				if strings.Contains(mysqlErr.Message, "product_type_id") {
-					errorResponse.Message = "invalid product_type_id"
-				} else if strings.Contains(mysqlErr.Message, "seller_id") {
-					errorResponse.Message = "invalid seller_id"
-				}
-				return nil, errorResponse
-			}
-		}
-		return nil, pkg.ServiceErrors[pkg.ErrInternalServer]
+		return nil, errorGenerator(err)
 	}
 
 	lastId, errId := result.LastInsertId()
@@ -189,24 +184,51 @@ func (p ProductSql) Create(product models.Product) (*models.Product, error) {
 }
 
 func (p ProductSql) Update(id int, product models.Product) error {
-	//TODO implement me
-	panic("implement me")
+	result, err := p.db.Exec(Update,
+		product.ProductCode,
+		product.Description,
+		product.NetWeight,
+		product.ExpirationRate,
+		product.RecommendedFreezingTemperature,
+		product.FreezingRate,
+		product.ProductTypeId,
+		product.SellerId,
+		product.Width,
+		product.Height,
+		product.Length,
+		product.ID,
+	)
+
+	if err != nil {
+		return errorGenerator(err)
+	}
+
+	rAffected, _ := result.RowsAffected()
+	if rAffected == 0 {
+		return pkg.ServiceErrors[pkg.ErrNotFound]
+	}
+
+	return nil
+
 }
 
-//func (p *ProductSql) validateProductCode(product models.Product) error {
-//	for _, value := range *p.db {
-//		if value.ID == product.ID {
-//			continue
-//		}
-//
-//		if strings.ToLower(*value.ProductCode) == strings.ToLower(*product.ProductCode) {
-//			return pkg.ServiceError{
-//				Code:         0,
-//				ResponseCode: http.StatusConflict,
-//				Message:      "Product code already exist",
-//			}
-//		}
-//	}
-//	return nil
-//}
-//
+func errorGenerator(err error) pkg.ServiceError {
+	var errorResponse pkg.ServiceError
+	if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+		switch mysqlErr.Number {
+		case 1062:
+			errorResponse = pkg.ServiceErrors[pkg.ErrConflict]
+			errorResponse.Message = "product_code already exists"
+			return errorResponse
+		case 1452:
+			errorResponse = pkg.ServiceErrors[pkg.ErrNotFound]
+			if strings.Contains(mysqlErr.Message, "product_type_id") {
+				errorResponse.Message = "invalid product_type_id"
+			} else if strings.Contains(mysqlErr.Message, "seller_id") {
+				errorResponse.Message = "invalid seller_id"
+			}
+			return errorResponse
+		}
+	}
+	return pkg.ServiceErrors[pkg.ErrInternalServer]
+}
