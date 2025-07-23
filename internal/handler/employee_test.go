27ec263febf7c7a8f1f8173417ad6e5a57d2d3a1
catriel escobar <catriel.escobar@mercadolibre.com>
2025-07-23 17:53:17 -0300

    test: enhance employee handler tests with additional CRUD operations and error handling

--- internal/handler/employee_test.go
+++ internal/handler/employee_test.go
@@ -5,10 +5,12 @@ import (
 	"app/pkg/models"
 	"app/test/employee"
 	"bytes"
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/require"
 )
 
@@ -42,7 +44,7 @@ func TestCreateEmployee(t *testing.T) {
 		}`
 
 		reqBody := bytes.NewReader([]byte(body))
-		req := httptest.NewRequest(http.MethodPost, "/api/v1/employee", reqBody)
+		req := httptest.NewRequest(http.MethodPost, "/api/v1/employees", reqBody)
 		res := httptest.NewRecorder()
 		hd.CreateEmployee(res, req)
 		expected := `{
@@ -66,7 +68,7 @@ func TestCreateEmployee(t *testing.T) {
 		"first_name": "Carlos"
 		}`
 		reqBody := bytes.NewReader([]byte(body))
-		req := httptest.NewRequest(http.MethodPost, "/api/v1/employee", reqBody)
+		req := httptest.NewRequest(http.MethodPost, "/api/v1/employees", reqBody)
 		res := httptest.NewRecorder()
 		hd.CreateEmployee(res, req)
 		expected := `{
@@ -99,7 +101,7 @@ func TestCreateEmployee(t *testing.T) {
 		}`
 
 		reqBody := bytes.NewReader([]byte(body))
-		req := httptest.NewRequest(http.MethodPost, "/api/v1/employee", reqBody)
+		req := httptest.NewRequest(http.MethodPost, "/api/v1/employees", reqBody)
 		res := httptest.NewRecorder()
 
 		hd.CreateEmployee(res, req)
@@ -136,7 +138,7 @@ func TestFindEmployee(t *testing.T) {
 		)
 		hd := NewEmployeeHandler(mockService)
 
-		req := httptest.NewRequest(http.MethodGet, "/api/v1/employee", nil)
+		req := httptest.NewRequest(http.MethodGet, "/api/v1/employees", nil)
 		res := httptest.NewRecorder()
 		expected := `{
     "data": [
@@ -154,5 +156,199 @@ func TestFindEmployee(t *testing.T) {
 		// then
 		require.Equal(t, expectedCode, res.Code)
 		require.JSONEq(t, expected, res.Body.String())
+	})
+	t.Run("find_by_id_non_existent 404", func(t *testing.T) {
+		mockService := new(employee.MockEmployeeService)
+		mockService.On("FindById", 3).Return(
+			models.Employee{},
+			pkg.ServiceErrors[pkg.ErrNotFound],
+		)
+		hd := NewEmployeeHandler(mockService)
+
+		req := httptest.NewRequest(http.MethodGet, "/api/v1/employees/3", nil)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "3")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeCtx))
+		res := httptest.NewRecorder()
+		expected := `{"message":"error: Not found", "status":"Not Found"}`
+		expectedCode := 404
+		hd.GetEmployee(res, req)
+		// then
+		require.Equal(t, expectedCode, res.Code)
+		require.JSONEq(t, expected, res.Body.String())
+	})
+
+	t.Run("find_by_id_existent", func(t *testing.T) {
+		mockService := new(employee.MockEmployeeService)
+		cardNumberID := "11223342"
+		firstName := "Carlos"
+		lastName := "López"
+		warehouseID := 3
+		id := 3
+
+		mockService.On("FindById", 3).Return(
+			models.Employee{
+				ID:           &id,
+				CardNumberID: &cardNumberID,
+				FirstName:    &firstName,
+				LastName:     &lastName,
+				WarehouseID:  &warehouseID,
+			},
+			nil,
+		)
+		hd := NewEmployeeHandler(mockService)
+
+		req := httptest.NewRequest(http.MethodGet, "/api/v1/employees/3", nil)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "3")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeCtx))
+		res := httptest.NewRecorder()
+		expected := `{
+	"data": {
+	    "id": 3,
+	    "card_number_id": "11223342",
+	    "first_name": "Carlos",
+	    "last_name": "López",
+	    "warehouse_id": 3
+	}
+	}`
+		expectedCode := 200
+		hd.GetEmployee(res, req)
+		// then
+		require.Equal(t, expectedCode, res.Code)
+		require.JSONEq(t, expected, res.Body.String())
+	})
+}
+
+func TestUpdateEmployee(t *testing.T) {
+	t.Run("update_ok", func(t *testing.T) {
+		mockService := new(employee.MockEmployeeService)
+		cardNumberID := "11223342"
+		firstName := "Carlos"
+		lastName := "López"
+		warehouseID := 3
+		id := 3
+
+		mockService.On("Update", models.Employee{
+			ID:           nil,
+			CardNumberID: &cardNumberID,
+			FirstName:    &firstName,
+			LastName:     &lastName,
+			WarehouseID:  &warehouseID,
+		}, 3).Return(
+			models.Employee{
+				ID:           &id,
+				CardNumberID: &cardNumberID,
+				FirstName:    &firstName,
+				LastName:     &lastName,
+				WarehouseID:  &warehouseID,
+			},
+			nil,
+		)
+		hd := NewEmployeeHandler(mockService)
+
+		body := `{
+		"card_number_id": "11223342",
+		"first_name": "Carlos",
+		"last_name": "López",
+		"warehouse_id": 3
+		}`
+		reqBody := bytes.NewReader([]byte(body))
+		req := httptest.NewRequest(http.MethodPatch, "/api/v1/employees/3", reqBody)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "3")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeCtx))
+		res := httptest.NewRecorder()
+		expected := `{
+    "data": {
+        "id": 3,
+        "card_number_id": "11223342",
+        "first_name": "Carlos",
+        "last_name": "López",
+        "warehouse_id": 3
+    }
+}`
+		expectedCode := 200
+		hd.UpdateEmployee(res, req)
+		// then
+		require.Equal(t, expectedCode, res.Code)
+		require.JSONEq(t, expected, res.Body.String())
+	})
+	t.Run("update_non_existent", func(t *testing.T) {
+		mockService := new(employee.MockEmployeeService)
+		cardNumberID := "11223342"
+		firstName := "Carlos"
+		lastName := "López"
+		warehouseID := 3
+
+		mockService.On("Update", models.Employee{
+			ID:           nil,
+			CardNumberID: &cardNumberID,
+			FirstName:    &firstName,
+			LastName:     &lastName,
+			WarehouseID:  &warehouseID,
+		}, 3).Return(
+			models.Employee{},
+			pkg.ServiceErrors[pkg.ErrNotFound],
+		)
+		hd := NewEmployeeHandler(mockService)
+
+		body := `{
+		"card_number_id": "11223342",
+		"first_name": "Carlos",
+		"last_name": "López",
+		"warehouse_id": 3
+		}`
+		reqBody := bytes.NewReader([]byte(body))
+		req := httptest.NewRequest(http.MethodPatch, "/api/v1/employees/3", reqBody)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "3")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeCtx))
+		res := httptest.NewRecorder()
+		expected := `{"message":"error: Not found", "status":"Not Found"}`
+		expectedCode := 404
+		hd.UpdateEmployee(res, req)
+		// then
+		require.Equal(t, expectedCode, res.Code)
+		require.JSONEq(t, expected, res.Body.String())
+	})
+}
+
+func TestDeleteEmployee(t *testing.T) {
+	t.Run("delete_non_existent", func(t *testing.T) {
+		mockService := new(employee.MockEmployeeService)
+		mockService.On("Delete", 3).Return(
+			pkg.ServiceErrors[pkg.ErrNotFound],
+		)
+		hd := NewEmployeeHandler(mockService)
+		req := httptest.NewRequest(http.MethodDelete, "/api/v1/employees/3", nil)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "3")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeCtx))
+		res := httptest.NewRecorder()
+		expected := `{"message":"error: Not found", "status":"Not Found"}`
+		expectedCode := 404
+		hd.DeleteEmployee(res, req)
+		// then
+		require.Equal(t, expectedCode, res.Code)
+		require.JSONEq(t, expected, res.Body.String())
+	})
+
+	t.Run("delete_ok", func(t *testing.T) {
+		mockService := new(employee.MockEmployeeService)
+		mockService.On("Delete", 3).Return(
+			nil,
+		)
+		hd := NewEmployeeHandler(mockService)
+		req := httptest.NewRequest(http.MethodDelete, "/api/v1/employees/3", nil)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "3")
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeCtx))
+		res := httptest.NewRecorder()
+
+		expectedCode := 204
+		hd.DeleteEmployee(res, req)
+		// then
+		require.Equal(t, expectedCode, res.Code)
 	})
 }
