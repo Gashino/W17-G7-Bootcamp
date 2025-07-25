27212b2147869d2c28e0e0ede4157037d45068cb
agusilva_meli <agustin.silva@mercadolibre.com>
2025-07-25 12:28:35 -0300

    test: add mock implementations for CarryRepository and CarryService

---
+++ test/carry/mock_carry_repository.go
@@ -0,0 +1,26 @@
+package warehouse
+
+import (
+	"app/pkg/models"
+
+	"github.com/stretchr/testify/mock"
+)
+
+/*
+	Create(v models.Carry) (c models.Carry, err error)
+	SearchByLocality(locality_id int) (v map[string]models.CarryByLocality, err error)
+*/
+
+type MockCarryRepository struct {
+	mock.Mock
+}
+
+func (m *MockCarryRepository) Create(v models.Carry) (w models.Carry, err error) {
+	args := m.Called(v)
+	return args.Get(0).(models.Carry), args.Error(1)
+}
+
+func (m *MockCarryRepository) SearchByLocality(locality_id int) (v map[string]models.CarryByLocality, err error) {
+	args := m.Called()
+	return args.Get(0).(map[string]models.CarryByLocality), args.Error(1)
+}
